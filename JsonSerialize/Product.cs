1f0ec991a5949355ebf2edc4ad97300b2a76e555
Zaman Safarov <zamanjs@code.edu.az>
2024-04-24 04:27:22 +0400

    first commit

---
+++ JsonSerialize/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JsonSerialize
+{
+    [Serializable]
+    public class Product : ISerializable
+    {
+        public int Id { get;private set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        static int counter = 0;
+        public Product()
+        {
+            counter++;
+            Id = counter;
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id,typeof(Product));
+            info.AddValue("Name", Name, typeof(Product));
+            info.AddValue("Surnam", Surname, typeof(Product));
+            info.AddValue("Position", Position, typeof(Product));
+        }
+
+        public override string? ToString()
+        {
+            return $"Id: {Id} Name: {Name} Surname: {Surname} Position: {Position}";
+        }
+    }
+}
---
+++ JsonSerialize/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace JsonSerialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "Zaman";
+            product.Surname = "Safarov";
+            product.Position = "Middle Front";
+
+            // Serialize the product to JSON
+            ProductToJSON(product);
+
+            // Deserialize the product from JSON
+            Product deserializedProduct = JSONToProduct();
+            Console.WriteLine(deserializedProduct);
+        }
+        public static void ProductToJSON(Product product)
+        {
+            string path = "D:\\Users\\User\\Desktop\\data.json";
+
+            // Use 'using' statement for proper resource disposal
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                JsonSerializer.Serialize(stream,product);
+            }
+        }
+
+        public static Product JSONToProduct()
+        {
+            string path = "D:\\Users\\User\\Desktop\\data.json";
+
+            // Use 'using' statement for proper resource disposal
+            using (Stream stream = new FileStream(path, FileMode.Open))
+            {
+               
+                return JsonSerializer.Deserialize<Product>(stream);
+            }
+        }
+    }
+}
